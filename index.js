c93161a0634af12b5743d4a142c5afb47be9f5ff
Manrique Buso - Garcia <manrique.buso@gmail.com>
2018-11-03 18:44:16 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,3 +13,8 @@ function deleteFromObjectByKey(object, key) {
   var newObj = Object.assign({}, key)
   return delete newObj.key
 }
+
+function destructivelyDeleteFromObjectByKey(object, key) {
+  delete object.key
+  return object
+}
